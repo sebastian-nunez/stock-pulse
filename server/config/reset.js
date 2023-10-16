ed5d468d6a1ac81b8fe33c2b2bff693e91accfa6
sebastian-nunez <02snunez@gmail.com>
2023-10-16 13:00:20 -0400

    user_table now resets between runs

--- server/config/reset.js
+++ server/config/reset.js
@@ -1,1 +1,49 @@
-console.log("reset!");
+import { userData } from "../data/users.js";
+import { pool } from "../config/database.js";
+
+const createUserTable = async () => {
+  const createQuery = `
+      DROP TABLE IF EXISTS user_table;
+
+      CREATE TABLE IF NOT EXISTS user_table (
+        id                  SERIAL PRIMARY KEY,
+        firstname           VARCHAR(50) NOT NULL,
+        lastname            VARCHAR(50) NOT NULL,
+        role                VARCHAR(25) NOT NULL,
+        image               VARCHAR(255)
+      );
+    `;
+
+  try {
+    await pool.query(createQuery);
+    console.log("Created the user_table!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const seedUsers = async () => {
+  const insertQuery = `
+      INSERT INTO user_table (firstname, lastname, role, image)
+      VALUES ($1, $2, $3, $4);
+    `;
+
+  userData.map(async user => {
+    try {
+      await pool.query(insertQuery, [
+        user.firstname,
+        user.lastname,
+        user.role,
+        user.image
+      ]);
+      console.log(
+        `Inserted ${user.firstname} ${user.lastname} into user_table!`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+await createUserTable();
+await seedUsers();
---
+++ server/data/users.js
@@ -0,0 +1,16 @@
+export const userData = [
+  {
+    firstname: "Sebastian",
+    lastname: "Nunez",
+    role: "Software Engineer",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQHbbpsnkcHQVg/profile-displayphoto-shrink_400_400/0/1696815844660?e=1703116800&v=beta&t=YTc0bSEAZiwLb2GVPevzAPXcSEty_1hQ9qSs2EVib4U"
+  },
+  {
+    firstname: "Priscilla",
+    lastname: "Colon",
+    role: "Software Engineer",
+    image:
+      "https://media.licdn.com/dms/image/C4E03AQFa9hyg4r8l4A/profile-displayphoto-shrink_400_400/0/1662519591069?e=1703116800&v=beta&t=FvoDov6jEfoY_U6W-kS6nr1_Vh3vBF6K3f_OD3YfPBM"
+  }
+];
