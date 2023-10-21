a5a10aeb2884bae537abe84ceb01a72141d2e800
sebastian-nunez <02snunez@gmail.com>
2023-10-21 19:30:19 -0400

    added a basic "search bar" for users

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import { Button } from "@nextui-org/react";
 import { Card, User, CardBody } from "@nextui-org/react";
 import { Users } from "lucide-react";
-import { useState } from "react";
 import UsersAPI from "../services/UsersAPI";
 import { useQuery } from "react-query";
+import { Search } from "lucide-react";
+import { Input } from "@nextui-org/react";
+import { useState } from "react";
 
 const Home = () => {
   const usersQuery = useQuery(["users"], UsersAPI.getAllUsers, {
     enabled: false,
   });
   const users = usersQuery.data;
+
+  const [searchInput, setSearchInput] = useState("");
 
   return (
     <>
@@ -30,24 +34,48 @@ const Home = () => {
         </Button>
       </section>
 
-      <section className="m-12 grid grid-cols-2 gap-6">
+      <div className="my-6 flex justify-end gap-6">
+        {usersQuery.isSuccess && (
+          <Input
+            type="text"
+            name="name"
+            placeholder="Benito Martinez"
+            variant="bordered"
+            startContent={<Search className="text-default-400" />}
+            className="mr-6 w-1/4"
+            value={searchInput}
+            onValueChange={setSearchInput}
+          />
+        )}
+      </div>
+
+      <section className="mx-6 grid grid-cols-2 gap-6">
         {users?.length > 0
-          ? users.map((user) => (
-              <Card key={user.id}>
-                <CardBody>
-                  <User
-                    name={`${user.firstname} ${user.lastname}`}
-                    description={user.role}
-                    avatarProps={{
-                      src: user.image,
-                      size: "lg",
-                      isBordered: true,
-                      isFocusable: true,
-                    }}
-                  />
-                </CardBody>
-              </Card>
-            ))
+          ? users
+              .filter((user) => {
+                const normalizedInput = searchInput.trim().toLowerCase();
+                const fullName = `${user.firstname} ${user.lastname}`
+                  .trim()
+                  .toLowerCase();
+
+                return fullName.includes(normalizedInput);
+              })
+              .map((user) => (
+                <Card key={user.id}>
+                  <CardBody>
+                    <User
+                      name={`${user.firstname} ${user.lastname}`}
+                      description={user.role}
+                      avatarProps={{
+                        src: user.image,
+                        size: "lg",
+                        isBordered: true,
+                        isFocusable: true,
+                      }}
+                    />
+                  </CardBody>
+                </Card>
+              ))
           : usersQuery.isError && <div>ERROR: {usersQuery.error?.message}</div>}
       </section>
     </>
