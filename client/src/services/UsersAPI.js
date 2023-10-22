6112a699ddc82e921abe6cf60b4ce3b88f9c6223
sebastian-nunez <02snunez@gmail.com>
2023-10-22 00:15:15 -0400

    users api now uses fetch directly (there was a bug with useQuery: the body was not being passed down)

--- client/src/services/UsersAPI.js
+++ client/src/services/UsersAPI.js
@@ -1,8 +1,26 @@
-import { requestJSON } from "../utils/api";
+const BASE_USERS_URL = "/api/users";
 
 class UsersAPI {
   static getAllUsers = async () => {
-    return await requestJSON("GET", "/api/users");
+    const res = await fetch(BASE_USERS_URL);
+    const users = await res.json();
+
+    return users;
+  };
+
+  static createUser = async (user) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    };
+
+    const res = await fetch(BASE_USERS_URL, options);
+    const createdUser = await res.json();
+
+    return createdUser;
   };
 }
 
