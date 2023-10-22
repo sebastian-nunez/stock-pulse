79763ce44b4c54dd15c57b9bb72ff8041410e839
sebastian-nunez <02snunez@gmail.com>
2023-10-22 02:29:34 -0400

    added a global toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,20 @@ import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter } from "react-router-dom";
+import toast from "react-hot-toast";
+import { QueryClient, QueryClientProvider, QueryCache } from "react-query";
 
 // react-query
-import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
     },
   },
+  queryCache: new QueryCache({
+    // global error handling
+    onError: (error) => toast.error(`Error: ${error.message}`),
+  }),
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
