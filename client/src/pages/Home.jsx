677511d6640f5b942c649a2e1fafa1a29d8e463a
sebastian-nunez <02snunez@gmail.com>
2023-10-22 00:16:12 -0400

    added a "add user" modal with a form

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,21 +2,68 @@ import { Button } from "@nextui-org/react";
 import { Card, User, CardBody } from "@nextui-org/react";
 import { Users } from "lucide-react";
 import UsersAPI from "../services/UsersAPI";
-import { useQuery } from "react-query";
-import { Search } from "lucide-react";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { Search, UserPlus2 } from "lucide-react";
 import { Input } from "@nextui-org/react";
 import { useState } from "react";
+import { Tooltip } from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
+
+const DEFAULT_USER = {
+  firstname: null,
+  lastname: null,
+  role: null,
+  image: null,
+};
 
 const Home = () => {
+  // modal
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  // use query
+  const queryClient = useQueryClient();
+
+  const createUser = useMutation(UsersAPI.createUser, () => {
+    queryClient.invalidateQueries(["users"]);
+  });
+
   const usersQuery = useQuery(["users"], UsersAPI.getAllUsers, {
     enabled: false,
   });
   const users = usersQuery.data;
 
+  // state
   const [searchInput, setSearchInput] = useState("");
+  const [user, setUser] = useState(DEFAULT_USER);
+
+  // form submission
+  const handleAddUser = (e) => {
+    e.preventDefault();
+
+    createUser.mutate(user);
+    setUser(DEFAULT_USER);
+  };
+
+  // form fields
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
 
   return (
     <>
+      {/* ------------ Heading -------------- */}
       <section className="text-center">
         <h1 className="mb-6 mt-12 text-6xl font-extrabold tracking-tighter">
           StockPulse
@@ -34,21 +81,34 @@ const Home = () => {
         </Button>
       </section>
 
-      <div className="my-6 flex justify-end gap-6">
-        {usersQuery.isSuccess && (
+      {/* ------------ Filter Options -------------- */}
+      {usersQuery.isSuccess && (
+        <div className="my-6 flex justify-end gap-6">
+          <Tooltip content="Add a User" delay={300}>
+            <Button
+              radius="sm"
+              variant="flat"
+              onPress={onOpen}
+              isDisabled={createUser.isLoading}
+            >
+              <UserPlus2 />
+            </Button>
+          </Tooltip>
+
           <Input
             type="text"
             name="name"
-            placeholder="Benito Martinez"
+            placeholder="Tim Cook"
             variant="bordered"
             startContent={<Search className="text-default-400" />}
             className="mr-6 w-1/4"
             value={searchInput}
             onValueChange={setSearchInput}
           />
-        )}
-      </div>
+        </div>
+      )}
 
+      {/* ------------ User Grid -------------- */}
       <section className="mx-6 grid grid-cols-2 gap-6">
         {users?.length > 0
           ? users
@@ -78,6 +138,75 @@ const Home = () => {
               ))
           : usersQuery.isError && <div>ERROR: {usersQuery.error?.message}</div>}
       </section>
+
+      {/* ------------ Modal -------------- */}
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <form onSubmit={handleAddUser}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Add User
+                </ModalHeader>
+
+                <ModalBody>
+                  <div className="flex gap-3">
+                    <Input
+                      type="text"
+                      name="firstname"
+                      label="Firstname"
+                      variant="bordered"
+                      onChange={handleChange}
+                      isRequired
+                    />
+                    <Input
+                      type="text"
+                      name="lastname"
+                      label="Lastname"
+                      variant="bordered"
+                      onChange={handleChange}
+                      isRequired
+                    />
+                  </div>
+
+                  <Input
+                    type="text"
+                    name="role"
+                    label="Role"
+                    variant="bordered"
+                    onChange={handleChange}
+                    isRequired
+                  />
+                  <Input
+                    type="text"
+                    name="image"
+                    label="Image Link"
+                    onChange={handleChange}
+                    variant="bordered"
+                    isClearable
+                    description="Please provide a valid URL"
+                  />
+                </ModalBody>
+
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Cancel
+                  </Button>
+
+                  <Button
+                    color="primary"
+                    variant="shadow"
+                    onPress={onClose}
+                    type="submit"
+                  >
+                    Add
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </form>
+      </Modal>
     </>
   );
 };
