216f2afe9944dd316fb327c651a86c4b6ffd0a73
sebastian-nunez <02snunez@gmail.com>
2023-10-22 01:53:19 -0400

    converted fetch to axios for improved error handling

--- client/src/services/UsersAPI.js
+++ client/src/services/UsersAPI.js
@@ -1,26 +1,25 @@
+import axios from "axios";
+
 const BASE_USERS_URL = "/api/users";
 
 class UsersAPI {
   static getAllUsers = async () => {
-    const res = await fetch(BASE_USERS_URL);
-    const users = await res.json();
+    const res = await axios.get(BASE_USERS_URL);
+    const users = res.data;
 
     return users;
   };
 
   static createUser = async (user) => {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
+    const body = JSON.stringify(user);
+    const headers = {
+      "Content-Type": "application/json",
     };
 
-    const res = await fetch(BASE_USERS_URL, options);
-    const createdUser = await res.json();
+    const res = await axios.post(BASE_USERS_URL, body, { headers });
+    const userCreated = res.data;
 
-    return createdUser;
+    return userCreated;
   };
 }
 
