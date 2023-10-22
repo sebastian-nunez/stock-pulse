c32b6d3b6c8e6c35de9b33b3b767b115f78b14ca
sebastian-nunez <02snunez@gmail.com>
2023-10-22 02:29:22 -0400

    added more userdata

--- server/data/users.js
+++ server/data/users.js
@@ -12,5 +12,281 @@ export const userData = [
     role: "Software Engineer",
     image:
       "https://media.licdn.com/dms/image/C4E03AQFa9hyg4r8l4A/profile-displayphoto-shrink_400_400/0/1662519591069?e=1703116800&v=beta&t=FvoDov6jEfoY_U6W-kS6nr1_Vh3vBF6K3f_OD3YfPBM"
+  },
+  {
+    firstname: "John",
+    lastname: "Doe",
+    role: "Frontend Developer",
+    image: "https://example.com/johndoe.jpg"
+  },
+  {
+    firstname: "Jane",
+    lastname: "Smith",
+    role: "Backend Developer",
+    image: "https://example.com/janesmith.jpg"
+  },
+  {
+    firstname: "David",
+    lastname: "Johnson",
+    role: "Data Scientist",
+    image: "https://example.com/davidjohnson.jpg"
+  },
+  {
+    firstname: "Emily",
+    lastname: "Brown",
+    role: "UX Designer",
+    image: "https://example.com/emilybrown.jpg"
+  },
+  {
+    firstname: "Michael",
+    lastname: "Wilson",
+    role: "DevOps Engineer",
+    image: "https://example.com/michaelwilson.jpg"
+  },
+  {
+    firstname: "Sophia",
+    lastname: "Lee",
+    role: "Product Manager",
+    image: "https://example.com/sophialee.jpg"
+  },
+  {
+    firstname: "William",
+    lastname: "Clark",
+    role: "QA Tester",
+    image: "https://example.com/williamclark.jpg"
+  },
+  {
+    firstname: "Olivia",
+    lastname: "Davis",
+    role: "Database Administrator",
+    image: "https://example.com/oliviadavis.jpg"
+  },
+  {
+    firstname: "James",
+    lastname: "Harris",
+    role: "System Architect",
+    image: "https://example.com/jamesharris.jpg"
+  },
+  {
+    firstname: "Lily",
+    lastname: "Walker",
+    role: "Graphic Designer",
+    image: "https://example.com/lilywalker.jpg"
+  },
+  {
+    firstname: "Benjamin",
+    lastname: "Hall",
+    role: "Network Engineer",
+    image: "https://example.com/benjaminhall.jpg"
+  },
+  {
+    firstname: "Ava",
+    lastname: "Evans",
+    role: "Scrum Master",
+    image: "https://example.com/avaevans.jpg"
+  },
+  {
+    firstname: "Alexander",
+    lastname: "Young",
+    role: "Full Stack Developer",
+    image: "https://example.com/alexanderyoung.jpg"
+  },
+  {
+    firstname: "Mia",
+    lastname: "Moore",
+    role: "Data Analyst",
+    image: "https://example.com/miamoore.jpg"
+  },
+  {
+    firstname: "Ethan",
+    lastname: "Allen",
+    role: "UI Developer",
+    image: "https://example.com/ethanallen.jpg"
+  },
+  {
+    firstname: "Aria",
+    lastname: "Parker",
+    role: "Business Analyst",
+    image: "https://example.com/ariaparker.jpg"
+  },
+  {
+    firstname: "Daniel",
+    lastname: "Carter",
+    role: "Mobile App Developer",
+    image: "https://example.com/danielcarter.jpg"
+  },
+  {
+    firstname: "Sofia",
+    lastname: "Baker",
+    role: "Project Manager",
+    image: "https://example.com/sofiabaker.jpg"
+  },
+  {
+    firstname: "Matthew",
+    lastname: "Adams",
+    role: "Software Tester",
+    image: "https://example.com/matthewadams.jpg"
+  },
+  {
+    firstname: "Grace",
+    lastname: "Wright",
+    role: "DevOps Engineer",
+    image: "https://example.com/gracewright.jpg"
+  },
+  {
+    firstname: "David",
+    lastname: "Lewis",
+    role: "Frontend Developer",
+    image: "https://example.com/davidlewis.jpg"
+  },
+  {
+    firstname: "Emma",
+    lastname: "Morris",
+    role: "Backend Developer",
+    image: "https://example.com/emmamorris.jpg"
+  },
+  {
+    firstname: "Oliver",
+    lastname: "Scott",
+    role: "Data Scientist",
+    image: "https://example.com/oliverscott.jpg"
+  },
+  {
+    firstname: "Ava",
+    lastname: "Perez",
+    role: "UX Designer",
+    image: "https://example.com/avaperez.jpg"
+  },
+  {
+    firstname: "Liam",
+    lastname: "Robinson",
+    role: "Product Manager",
+    image: "https://example.com/liamrobinson.jpg"
+  },
+  {
+    firstname: "Harper",
+    lastname: "Alexander",
+    role: "QA Tester",
+    image: "https://example.com/harperalexander.jpg"
+  },
+  {
+    firstname: "Lucas",
+    lastname: "Ward",
+    role: "Database Administrator",
+    image: "https://example.com/lucasward.jpg"
+  },
+  {
+    firstname: "Amelia",
+    lastname: "Watson",
+    role: "System Architect",
+    image: "https://example.com/ameliawatson.jpg"
+  },
+  {
+    firstname: "Logan",
+    lastname: "Stewart",
+    role: "Graphic Designer",
+    image: "https://example.com/loganstewart.jpg"
+  },
+  {
+    firstname: "Sophia",
+    lastname: "Turner",
+    role: "Network Engineer",
+    image: "https://example.com/sophiaturner.jpg"
+  },
+  {
+    firstname: "Jackson",
+    lastname: "Garcia",
+    role: "Scrum Master",
+    image: "https://example.com/jacksongarcia.jpg"
+  },
+  {
+    firstname: "Avery",
+    lastname: "Lopez",
+    role: "Full Stack Developer",
+    image: "https://example.com/averylopez.jpg"
+  },
+  {
+    firstname: "Evelyn",
+    lastname: "Hall",
+    role: "Data Analyst",
+    image: "https://example.com/evelynhall.jpg"
+  },
+  {
+    firstname: "Noah",
+    lastname: "Morgan",
+    role: "UI Developer",
+    image: "https://example.com/noahmorgan.jpg"
+  },
+  {
+    firstname: "Harper",
+    lastname: "Alexander",
+    role: "Business Analyst",
+    image: "https://example.com/harperalexander.jpg"
+  },
+  {
+    firstname: "Lucas",
+    lastname: "Ward",
+    role: "Mobile App Developer",
+    image: "https://example.com/lucasward.jpg"
+  },
+  {
+    firstname: "Emily",
+    lastname: "Brown",
+    role: "Project Manager",
+    image: "https://example.com/emilybrown.jpg"
+  },
+  {
+    firstname: "Aria",
+    lastname: "Parker",
+    role: "Software Tester",
+    image: "https://example.com/ariaparker.jpg"
+  },
+  {
+    firstname: "William",
+    lastname: "Clark",
+    role: "DevOps Engineer",
+    image: "https://example.com/williamclark.jpg"
+  },
+  {
+    firstname: "James",
+    lastname: "Harris",
+    role: "Frontend Developer",
+    image: "https://example.com/jamesharris.jpg"
+  },
+  {
+    firstname: "Lily",
+    lastname: "Walker",
+    role: "Backend Developer",
+    image: "https://example.com/lilywalker.jpg"
+  },
+  {
+    firstname: "Benjamin",
+    lastname: "Hall",
+    role: "Data Scientist",
+    image: "https://example.com/benjaminhall.jpg"
+  },
+  {
+    firstname: "Ava",
+    lastname: "Evans",
+    role: "UX Designer",
+    image: "https://example.com/avaevans.jpg"
+  },
+  {
+    firstname: "Alexander",
+    lastname: "Young",
+    role: "Product Manager",
+    image: "https://example.com/alexanderyoung.jpg"
+  },
+  {
+    firstname: "Mia",
+    lastname: "Moore",
+    role: "QA Tester",
+    image: "https://example.com/miamoore.jpg"
+  },
+  {
+    firstname: "Ethan",
+    lastname: "Allen",
+    role: "Database Administrator",
+    image: "https://example.com/ethanallen.jpg"
   }
 ];
