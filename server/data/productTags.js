6e5cde4fb5046d1feb9b9726762c7514bc5dd6d3
sebastian-nunez <02snunez@gmail.com>
2023-10-31 08:52:20 -0400

    added a products with multiple tags

--- server/data/productTags.js
+++ server/data/productTags.js
@@ -4,12 +4,20 @@ export const productTagData = [
     tag_id: 1
   },
   {
+    product_id: 1,
+    tag_id: 2
+  },
+  {
     product_id: 2,
     tag_id: 1
   },
   {
     product_id: 3,
     tag_id: 1
+  },
+  {
+    product_id: 3,
+    tag_id: 2
   },
   {
     product_id: 4,
