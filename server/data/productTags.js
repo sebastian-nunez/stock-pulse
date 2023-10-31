ca499bd4ab59c4571f03d84ad1ef8e265f6218b5
sebastian-nunez <02snunez@gmail.com>
2023-10-31 08:32:21 -0400

    Created some mock data for products, categories, tags etc.

---
+++ server/data/productTags.js
@@ -0,0 +1,22 @@
+export const productTagData = [
+  {
+    product_id: 1,
+    tag_id: 1
+  },
+  {
+    product_id: 2,
+    tag_id: 1
+  },
+  {
+    product_id: 3,
+    tag_id: 1
+  },
+  {
+    product_id: 4,
+    tag_id: 1
+  },
+  {
+    product_id: 5,
+    tag_id: 2
+  }
+];
---
+++ server/data/products.js
@@ -0,0 +1,283 @@
+export const productData = [
+  {
+    name: "AirForce 1 '07",
+    brand: "Nike",
+    description: "A classic shoe known and loved by most!",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-mens-shoes-jBrhbr.png",
+    quantity: 10,
+    price: 115,
+    is_available: true,
+    weight: 2.5,
+    dimensions: "10 x 5 x 3",
+    warranty_info: "One year limited warranty",
+    category_id: 1
+  },
+  {
+    name: "Sabrina 1 'Grounded",
+    brand: "Nike",
+    description: "Sabrina 1 'Grounded' Basketball Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/815bf12f-8128-41ef-a2ae-fdb687ae4335/sabrina-1-grounded-basketball-shoes-f8jr2H.png",
+    quantity: 57,
+    price: 160,
+    is_available: true,
+    weight: 2.0,
+    dimensions: "12 x 6 x 4",
+    warranty_info: "Two-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "LeBron XXI 'Dragon Pearl",
+    brand: "Nike",
+    description: "LeBron XXI 'Dragon Pearl' Basketball Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c9dc43a5-9e2e-441a-bb7a-10aaec1f167d/lebron-xxi-dragon-pearl-basketball-shoes-lnQSsH.png",
+    quantity: 49,
+    price: 130,
+    is_available: true,
+    weight: 2.2,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "Nike G.T. Hustle 2",
+    brand: "Nike",
+    description: "Nike G.T. Hustle 2 Men's Basketball Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2dffe315-2f26-4875-8e18-4ace07ad0715/gt-hustle-2-mens-basketball-shoes-SVKv6C.png",
+    quantity: 4,
+    price: 200,
+    is_available: true,
+    weight: 2.4,
+    dimensions: "11.5 x 6 x 4.5",
+    warranty_info: "One-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "LeBron NXXT Gen",
+    brand: "Nike",
+    description: "LeBron NXXT Gen Basketball Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/810505ff-475e-4d49-892e-1311e70528d6/lebron-nxxt-gen-basketball-shoes-55g4w1.png",
+    quantity: 47,
+    price: 170,
+    is_available: true,
+    weight: 2.3,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "Luka 2",
+    brand: "Nike",
+    description: "Luka 2 Basketball Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/0e35c961-428f-43ed-9a6e-cbcef3fe02bc/luka-2-basketball-shoes-vcXFrk.png",
+    quantity: 44,
+    price: 170,
+    is_available: true,
+    weight: 2.3,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "Tatum 1 'Wave Runner",
+    brand: "Nike",
+    description: "Tatum 1 'Wave Runner' Basketball Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/aae4d148-61a3-4436-8ba1-903a2b1202e7/tatum-1-wave-runner-basketball-shoes-98vPwk.png",
+    quantity: 34,
+    price: 130,
+    is_available: true,
+    weight: 2.2,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "Nike Dunk Mid Premium",
+    brand: "Nike",
+    description: "Nike Dunk Mid Premium Men's Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/78c69317-d438-4f6a-9f24-6878da125e5b/dunk-mid-premium-mens-shoes-SVQ6RD.png",
+    quantity: 3,
+    price: 120,
+    is_available: true,
+    weight: 2.0,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info:
+      "No warranty. This product is not eligible for returns or exchanges.",
+    category_id: 1
+  },
+  {
+    name: "Nike Air Force 1 '07 Premium",
+    brand: "Nike",
+    description: "Nike Air Force 1 '07 Premium Men's Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a6805b48-80dc-4c4c-be1f-7229bc4c6826/air-force-1-07-premium-mens-shoes-M1LkHr.png",
+    quantity: 31,
+    price: 140,
+    is_available: true,
+    weight: 2.2,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 1
+  },
+  {
+    name: "Nike Vomero 17",
+    brand: "Nike",
+    description: "Nike Vomero 17 Men's Road Running Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/cac247c8-c003-49a4-94c9-b073374be187/vomero-17-mens-road-running-shoes-ThMlMm.png",
+    quantity: 1,
+    price: 145,
+    is_available: false,
+    weight: 1.8,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 3
+  },
+  {
+    name: "Nike Invincible 3",
+    brand: "Nike",
+    description: "Nike Invincible 3 Men's Road Running Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/70dec0ae-c9c2-4556-b806-f291e19b7924/invincible-3-mens-road-running-shoes-4xHjXL.png",
+    quantity: 52,
+    price: 160,
+    is_available: true,
+    weight: 2.0,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "Two-year limited warranty",
+    category_id: 3
+  },
+  {
+    name: "Air Jordan 1 Mid SE",
+    brand: "Nike",
+    description: "Air Jordan 1 Mid SE Men's Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f895ad18-a196-454d-ae19-84800a531406/air-jordan-1-mid-se-mens-shoes-S9CPQh.png",
+    quantity: 14,
+    price: 180,
+    is_available: true,
+    weight: 2.5,
+    dimensions: "12 x 6 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 1
+  },
+  {
+    name: "Air Jordan 1 Retro High OG",
+    brand: "Nike",
+    description: "Air Jordan 1 Retro High OG Men's Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/6c2b1dc1-7614-4d1c-89d6-ab0bf6422a7f/air-jordan-1-retro-high-og-mens-shoes-gX9FS0.png",
+    quantity: 1,
+    price: 135,
+    is_available: false,
+    weight: 2.3,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 1
+  },
+  {
+    name: "Air Jordan 1 Element",
+    brand: "Nike",
+    description: "Air Jordan 1 Element Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/a79fb0e3-0459-4238-9697-60dda465ef2b/air-jordan-1-element-shoes-c5Dc04.png",
+    quantity: 1,
+    price: 200,
+    is_available: true,
+    weight: 2.3,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "Three-year limited warranty",
+    category_id: 1
+  },
+  {
+    name: "Air Jordan 1 Low OG",
+    brand: "Nike",
+    description: "Air Jordan 1 Low OG Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/e1f10ac9-74ae-42ef-bb39-33f94474c127/air-jordan-1-low-og-shoes-v0FbJt.png",
+    quantity: 15,
+    price: 200,
+    is_available: true,
+    weight: 2.3,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 1
+  },
+  {
+    name: "Nike Dunk Low Retro BG",
+    brand: "Nike",
+    description: "Nike Dunk Low Retro Men's Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/cfd82d2b-c8cc-4713-8b53-d305eb655e0c/dunk-low-retro-mens-shoes-76KnBL.png",
+    quantity: 23,
+    price: 140,
+    is_available: true,
+    weight: 2.0,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 1
+  },
+  {
+    name: "Nike Dunk Low Retro",
+    brand: "Nike",
+    description: "Nike Dunk Low Retro Men's Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c2643901-9e8c-41b5-b17d-36802f3102e9/dunk-low-retro-mens-shoes-M0t9Sv.png",
+    quantity: 35,
+    price: 115,
+    is_available: true,
+    weight: 2.2,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "Two-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "Nike P-6000 Premium",
+    brand: "Nike",
+    description: "Nike P-6000 Premium Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c3393350-4ea7-44bb-a3fd-dee4b7376884/p-6000-premium-shoes-XkgpKW.png",
+    quantity: 15,
+    price: 115,
+    is_available: true,
+    weight: 2.0,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "Nike Air Max 2013",
+    brand: "Nike",
+    description: "Nike Air Max 2013 Men's Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1e618b39-3ca0-4475-b3a3-48d925242803/air-max-2013-mens-shoes-qRrwhT.png",
+    quantity: 6,
+    price: 120,
+    is_available: true,
+    weight: 2.2,
+    dimensions: "11 x 5.5 x 4",
+    warranty_info: "One-year limited warranty",
+    category_id: 2
+  },
+  {
+    name: "Nike Air Max Pulse Roam",
+    brand: "Nike",
+    description: "Nike Air Max Pulse Roam Men's Shoes",
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f685abe-510a-4599-bb28-69859836bf88/air-max-pulse-roam-mens-shoes-X285L2.png",
+    quantity: 17,
+    price: 170,
+    is_available: true,
+    weight: 2.5,
+    dimensions: "12 x 6 x 4",
+    warranty_info: "Four-year limited warranty",
+    category_id: 2
+  }
+];
