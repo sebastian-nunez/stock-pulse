c004334f80ce48bed70dbfc24d33495447f38f61
priscillalynn <plynn9036@gmail.com>
2023-10-31 10:02:02 -0400

    imported category routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import UserRoutes from "./routes/users.js";
+import CategoryRoutes from "./routes/categories.js";
 
 // constants
 const PORT = process.env.PORT || 3001;
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/users", UserRoutes);
+app.use("/api/categories", CategoryRoutes);
 
 // health check endpoints
 app.get("/api", (req, res) => {
