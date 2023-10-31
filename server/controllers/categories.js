e40f05d9cb9ad930c99f67a81cef7a96d44b5abd
priscillalynn <plynn9036@gmail.com>
2023-10-31 12:49:04 -0400

    fixed category controller

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -4,80 +4,108 @@ import Category from "../models/categories.js";
 class CategoryController {
   static getCategories = async (req, res) => {
     try {
-      const categories = await Category.find();
+      const categories = await Category.getAll();
       res.status(200).json(categories);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(400).json({ message: error.message });
     }
   };
 
   static getCategoryById = async (req, res) => {
+    let { categoryId } = req.params;
+
     try {
-      const category = await Category.findById(req.params.id);
+      categoryId = parseInt(categoryId);
+
+      const category = await Category.getOneById(categoryId);
+
       if (!category) {
-        return res.status(404).json({ message: "Category not found" });
+        res.status(404).json({ message: "Category not found!" });
+        return;
       }
+
       res.status(200).json(category);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(400).json({ message: error.message });
     }
   };
 
   static getCategoryByName = async (req, res) => {
+    const { categoryName } = req.params;
+
     try {
-      const category = await Category.findOne({ name: req.params.name });
-      if (!category) {
-        return res.status(404).json({ message: "Category not found" });
+      const category = await Category.getOneByName(categoryName);
+
+      if (!tag) {
+        res.status(404).json({ message: "Category not found!" });
+        return;
       }
+
       res.status(200).json(category);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(400).json({ message: error.message });
     }
   };
 
   static deleteCategory = async (req, res) => {
+    let { categoryId } = req.params;
+
     try {
-      const category = await Category.findByIdAndDelete(req.params.id);
-      if (!category) {
-        return res.status(404).json({ message: "Category not found" });
+      categoryId = parseInt(categoryId);
+
+      const deletedCategory = await Category.deleteOne(categoryId);
+
+      if (!deletedCategory) {
+        res.status(404).json({ message: "Category not found!" });
+        return;
       }
-      res.status(200).json({ message: "Category deleted successfully" });
+
+      res
+        .status(200)
+        .json({ message: "Category deleted successfully!", deletedCategory });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(400).json({ message: error.message });
     }
   };
 
   static createCategory = async (req, res) => {
-    const { name, description } = req.body;
-    const category = new Category({
-      name,
-      description,
-    });
+    let { name, description } = req.body;
 
     try {
-      const newCategory = await category.save();
-      res.status(201).json(newCategory);
+      const createdCategory = await Category.createOne(name, description);
+
+      if (!createdCategory) {
+        res.status(409).json({ message: "Category already exists!" });
+        return;
+      }
+
+      res
+        .status(201)
+        .json({ message: "Category created successfully!", createdCategory });
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
   };
 
   static updateCategory = async (req, res) => {
+    let { categoryId } = req.params;
+    const { name, description } = req.body;
+
     try {
-      const category = await Category.findByIdAndUpdate(
-        req.params.id,
-        {
-          name: req.body.name,
-          description: req.body.description,
-        },
-        { new: true }
-      );
-      if (!category) {
-        return res.status(404).json({ message: "Category not found" });
+      categoryId = parseInt(categoryId);
+
+      const updateCategory = await Category.updateOne(categoryId, name, description);
+
+      if (!updateCategory) {
+        res.status(404).json({ message: "Category not found!" });
+        return;
       }
-      res.status(200).json(category);
+
+      res
+        .status(200)
+        .json({ message: "Category updated successfully!", updateCategory });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(400).json({ message: error.message });
     }
   };
 }
