e1ba6118b12ad32b9c6a701ecfa7809806b5d056
sebastian-nunez <02snunez@gmail.com>
2023-11-01 00:45:39 -0400

    code reformatted

--- server/controllers/productTag.js
+++ server/controllers/productTag.js
@@ -1,89 +1,99 @@
 import ProductTag from "../models/productTag.js";
 
 class ProductTagController {
-    static getProductTags = async (req, res) => {
-        try {
-            const productTags = await ProductTag.getAll();
+  static getProductTags = async (req, res) => {
+    try {
+      const productTags = await ProductTag.getAll();
 
-            if (!productTags) {
-                res.status(404).json({ message: "No product tags found!" });
-                return;
-            }
+      if (!productTags) {
+        res.status(404).json({ message: "No product tags found!" });
+        return;
+      }
 
-            res.status(200).json(productTags);
-        } catch (error) {
-            res.status(400).json({ message: error.message });
-        }
-    };
+      res.status(200).json(productTags);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
 
-    static getProductsWithTag = async (req, res) => {
-        const { tagId } = req.params;
+  static getProductsWithTag = async (req, res) => {
+    const { tagId } = req.params;
 
-        try {
-            const products = await ProductTag.getAllProducts(tagId);
+    try {
+      const products = await ProductTag.getAllProducts(tagId);
 
-            if (!products) {
-                res.status(404).json({ message: "No products found!" });
-                return;
-            }
+      if (!products) {
+        res.status(404).json({ message: "No products found!" });
+        return;
+      }
 
-            res.status(200).json(products);
-        } catch (error) {
-            res.status(400).json({ message: error.message });
-        }
-    };
+      res.status(200).json(products);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
 
-    static getTagsOfProduct = async (req, res) => {
-        const { productId } = req.params;
+  static getTagsOfProduct = async (req, res) => {
+    const { productId } = req.params;
 
-        try {
-            const tags = await ProductTag.getAllTags(productId);
+    try {
+      const tags = await ProductTag.getAllTags(productId);
 
-            if (!tags) {
-                res.status(404).json({ message: "No tags found!" });
-                return;
-            }
+      if (!tags) {
+        res.status(404).json({ message: "No tags found!" });
+        return;
+      }
 
-            res.status(200).json(tags);
-        } catch (error) {
-            res.status(400).json({ message: error.message });
-        }
-    };
+      res.status(200).json(tags);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
 
-    static deleteProductTag = async (req, res) => {
-        const { productId } = req.params;
+  static deleteProductTag = async (req, res) => {
+    let { productId, tagId } = req.body;
 
-        try {
-            const deletedProductTag = await ProductTag.deleteOne(productId);
+    try {
+      productId = parseInt(productId);
+      tagId = parseInt(tagId);
 
-            if (!deletedProductTag) {
-                res.status(404).json({ message: "Product tag not found!" });
-                return;
-            }
+      const deletedProductTag = await ProductTag.deleteOne(productId, tagId);
 
-            res.status(200).json(deletedProductTag);
-        } catch (error) {
-            res.status(400).json({ message: error.message });
-        }
-    };
+      if (!deletedProductTag) {
+        res.status(404).json({ message: "Product tag not found!" });
+        return;
+      }
 
-    static deleteAllTagsOfProduct = async (req, res) => {
-        const { productId } = req.params;
+      res.status(200).json({
+        message: "Product tag deleted successfully!",
+        deletedProductTag
+      });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
 
-        try {
-            const deletedProductTag = await ProductTag.deleteAllTags(productId);
+  static deleteAllTagsOfProduct = async (req, res) => {
+    let { productId } = req.params;
 
-            if (!deletedProductTag) {
-                res.status(404).json({ message: "Product tag not found!" });
-                return;
-            }
+    try {
+      productId = parseInt(productId);
 
-            res.status(200).json(deletedProductTag);
-        } catch (error) {
-            res.status(400).json({ message: error.message });
-        }
-    };
+      const deletedProductTag = await ProductTag.deleteAllTags(productId);
 
+      if (!deletedProductTag) {
+        res.status(404).json({ message: "Product tag not found!" });
+        return;
+      }
+
+      res.status(200).json({
+        message: `All tags for product with ID ${productId} were removed successfully!`,
+        deletedProductTag
+      });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
 }
 
 export default ProductTagController;
