a9da5399d5a7b81f818597aa0f38248aa253d724
sebastian-nunez <02snunez@gmail.com>
2023-11-01 00:43:21 -0400

    updated ProductTag routes to better align with API usage

--- server/routes/productTag.js
+++ server/routes/productTag.js
@@ -4,25 +4,16 @@ import ProductTagController from "../controllers/productTag.js";
 const router = express.Router();
 
 //get all products tags GET /api/products/product-tag
-router.get("/product-tag", ProductTagController.getProductTags);
+router.get("/", ProductTagController.getProductTags);
 //get products with a specific tag GET /api/products/product-tag/products/:tagId
-router.get(
-  "/product-tag/products/:tagId",
-  ProductTagController.getProductsWithTag
-);
+router.get("/products/:tagId", ProductTagController.getProductsWithTag);
 //get all tags of a specific product GET /api/products/product-tag/tags/:productId
-router.get(
-  "/product-tag/tags/:productId",
-  ProductTagController.getTagsOfProduct
-);
+router.get("/tags/:productId", ProductTagController.getTagsOfProduct);
 //create a product tag POST /api/products/product-tag
-router.post("/product-tag", ProductTagController.createProductTag);
+router.post("/", ProductTagController.createProductTag);
 //delete a product tag DELETE /api/products/product-tag
-router.delete("/:tagId", ProductTagController.deleteProductTag);
+router.delete("/", ProductTagController.deleteProductTag);
 //delete all tags of a specific product DELETE /api/products/product-tag/tags/:productId
-router.delete(
-  "/product-tag/tags/:productId",
-  ProductTagController.deleteAllTagsOfProduct
-);
+router.delete("/tags/:productId", ProductTagController.deleteAllTagsOfProduct);
 
 export default router;
