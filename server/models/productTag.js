4d6581e11318dc85bb94d1f2f094616f7fffd2f7
sebastian-nunez <02snunez@gmail.com>
2023-11-01 00:49:45 -0400

    updated parameter names to be consistent with the other models

--- server/models/productTag.js
+++ server/models/productTag.js
@@ -11,58 +11,58 @@ class ProductTagModel {
     return results.rows;
   };
 
-  static getAllProducts = async tag_id => {
+  static getAllProducts = async tagId => {
     const selectQuery = `
         SELECT *
         FROM product_tag
         WHERE tag_id = $1;
     `;
 
-    const results = await pool.query(selectQuery, [tag_id]);
+    const results = await pool.query(selectQuery, [tagId]);
     return results.rows;
   };
 
-  static getAllTags = async product_id => {
+  static getAllTags = async productId => {
     const selectQuery = `
         SELECT *
         FROM product_tag
         WHERE product_id = $1;
     `;
 
-    const results = await pool.query(selectQuery, [product_id]);
+    const results = await pool.query(selectQuery, [productId]);
     return results.rows;
   };
 
-  static createOne = async (product_id, tag_id) => {
+  static createOne = async (productId, tagId) => {
     const insertQuery = `
         INSERT INTO product_tag (product_id, tag_id)
         VALUES ($1, $2)
         RETURNING *;
     `;
 
-    const results = await pool.query(insertQuery, [product_id, tag_id]);
+    const results = await pool.query(insertQuery, [productId, tagId]);
     return results.rows[0];
   };
 
-  static deleteOne = async (product_id, tag_id) => {
+  static deleteOne = async (productId, tagId) => {
     const deleteQuery = `
         DELETE FROM product_tag
         WHERE (product_id, tag_id) = ($1, $2)
         RETURNING *;
     `;
 
-    const results = await pool.query(deleteQuery, [product_id, tag_id]);
+    const results = await pool.query(deleteQuery, [productId, tagId]);
     return results.rows[0];
   };
 
-  static deleteAllTags = async product_id => {
+  static deleteAllTags = async productId => {
     const deleteQuery = `
         DELETE FROM product_tag
         WHERE product_id = $1
         RETURNING *;
     `;
 
-    const results = await pool.query(deleteQuery, [product_id]);
+    const results = await pool.query(deleteQuery, [productId]);
     return results.rows[0];
   };
 }
