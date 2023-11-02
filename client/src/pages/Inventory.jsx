060e6300c2a6a484f6afb5cec5591de1f5b86f66
sebastian-nunez <02snunez@gmail.com>
2023-11-02 12:20:17 -0400

    minor refactor

--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -1,8 +1,10 @@
 const Inventory = () => {
   return (
-    <div className="p-6 text-center">
-      <h1 className="text-5xl font-extrabold tracking-tighter">Inventory</h1>
-      <p className="text-gray-500">We're working on it...</p>
+    <div>
+      <div className="text-center">
+        <h1 className="text-5xl font-extrabold tracking-tighter">Inventory</h1>
+        <p className="text-gray-500">We're working on it...</p>
+      </div>
     </div>
   );
 };
