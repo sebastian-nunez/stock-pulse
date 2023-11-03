5b863bf81bccf8e2f14c0718c397eab2ba4c5ed2
priscillalynn <plynn9036@gmail.com>
2023-11-02 20:20:53 -0400

    added our names to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,52 @@
+import React from "react";
+import { Layers3 } from "lucide-react";
+import "./Footer.css";
+
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <div className="footer">
+      <Layers3 />
+      <p>
+        Built by{" "}
+        <a
+          className="footer-links"
+          href="https://www.linkedin.com/in/sebastian-nunez-profile/"
+          target="blank"
+        >
+          Sebastian Nunez
+        </a>{" "}
+        and{" "}
+        <a
+          className="footer-links"
+          href="https://www.linkedin.com/in/priscillalynn/"
+          target="blank"
+        >
+          Priscilla Colon.
+        </a>
+      </p>
+      {/*
+        <p>StockPulse &copy;</p>
+        <div className="footer-wrap">
+        <p className="title">Title</p>
+        <p className="footer-details">Contact: info@example.com</p>
+        <p className="footer-details">
+          Address: 123 Main St, Cityville, State, 12345
+        </p>
+        <p className="footer-details">Phone: 123-456-7890</p>
+
+        <p className="title">Terms of Service</p>
+        <a href="" className="footer-details">
+          Link
+        </a>
+        <a href="" className="footer-details">
+          Link
+        </a>
+        <a href="" className="footer-details">
+          Link
+        </a>
+        </div>*/}
+    </div>
+  );
 };
 
 export default Footer;
