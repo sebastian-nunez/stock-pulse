bc3a9f20211d1f390d65eeb2cb2eb8ee4923023d
sebastian-nunez <02snunez@gmail.com>
2023-11-03 14:56:29 -0400

    product form now retrieves all tags and categories

--- client/src/components/ProductDetailsForm.jsx
+++ client/src/components/ProductDetailsForm.jsx
@@ -1,6 +1,15 @@
 import { Input } from "@nextui-org/react";
+import { useQuery } from "react-query";
+import CategoriesAPI from "../services/CategoriesAPI";
+import TagsAPI from "../services/TagsAPI";
 
 const ProductDetailsForm = ({ product, onFormChange, onSubmit }) => {
+  const categoriesQuery = useQuery(["categories"], CategoriesAPI.getCategories);
+  const categories = categoriesQuery.data;
+
+  const tagsQuery = useQuery(["tags"], TagsAPI.getAllTags);
+  const tags = tagsQuery.data;
+
   return (
     <form id="product-details-form" onChange={onFormChange} onSubmit={onSubmit}>
       <Input name="name" placeholder="Name" value={product.name} />
