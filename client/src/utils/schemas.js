7be779b1e308c5fe7192ec2a485f059e9e08d74a
sebastian-nunez <02snunez@gmail.com>
2023-11-04 08:40:13 -0400

    updated zod schema for products

--- client/src/utils/schemas.js
+++ client/src/utils/schemas.js
@@ -5,11 +5,11 @@ export const productSchema = z.object({
     .string()
     .min(5, "Product name must be at least 5 characters")
     .max(100, "Product name must be less than 100 characters"),
-  is_available: z.boolean("Product availability is required!"),
+  is_available: z.boolean("Product availability is required!").default(true),
   brand: z
     .string("Product brand is required!")
-    .min(5, "Product name must be at least 5 characters")
-    .max(100, "Product name must be less than 100 characters"),
+    .min(2, "Product brand must be at least 2 characters")
+    .max(100, "Product brand must be less than 100 characters"),
   price: z
     .string("Product price is required!")
     .refine((val) => !Number.isNaN(parseInt(val, 10)), {
@@ -27,21 +27,27 @@ export const productSchema = z.object({
     }),
   dimensions: z
     .string("Product dimensions are required!")
-    .max(50, "Product name must be less than 50 characters"),
+    .min(6, "Product dimensions must be at least 6 characters")
+    .max(50, "Product dimensions must be less than 50 characters"),
   description: z
     .string("Product description is required!")
-    .min(15, "Product name must be at least 15 characters")
-    .max(100, "Product name must be less than 100 characters"),
+    .min(15, "Product description must be at least 15 characters")
+    .max(100, "Product description must be less than 100 characters"),
   image: z
     .string("Product image is required!")
-    .min(10, "Product name must be at least 10 characters")
-    .max(255, "Product name must be less than 255 characters"),
+    .min(10, "Product image must be at least 10 characters")
+    .max(255, "Product image must be less than 255 characters"),
   notes: z
     .string("Product notes are required!")
-    .max(255, "Product name must be less than 255 characters"),
-  tags: z.array(z.string(), "Product tags are required!"),
+    .min(5, "Product notes must be at least 5 characters")
+    .max(255, "Product notes must be less than 255 characters")
+    .default("No notes available."),
+  tags: z.any(),
   warranty_info: z
     .string("Product warranty info is required!")
-    .max(255, "Product name must be less than 255 characters"),
+    .min(10, "Warranty information must be at least 10 characters")
+    .max(255, "Warranty information must be less than 255 characters")
+    .default("No warranty information available."),
+
   category: z.string("Product category is required!"),
 });
