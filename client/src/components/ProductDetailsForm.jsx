d4dcdf88ea4f3a92054f47b9e59e11c114180c18
sebastian-nunez <02snunez@gmail.com>
2023-11-07 06:50:40 -0500

    wrong html tag corrected

--- client/src/components/ProductDetailsForm.jsx
+++ client/src/components/ProductDetailsForm.jsx
@@ -41,7 +41,7 @@ const ProductDetailsForm = ({ product, onSubmit }) => {
     <form
       id="product-details-form"
       onSubmit={handleSubmit(onSubmit)}
-      novalidate="novalidate"
+      noValidate
       className="flex flex-col gap-3"
     >
       <div className="flex gap-3">
