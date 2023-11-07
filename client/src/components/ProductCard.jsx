a7a491cac74067d96a5b8b7fae5b7ba8cc2c083c
sebastian-nunez <02snunez@gmail.com>
2023-11-07 07:02:48 -0500

    created a basic prototype and linked up the modals with actions (edit, view)

---
+++ client/src/components/ProductCard.jsx
@@ -0,0 +1,67 @@
+import { Button, useDisclosure } from "@nextui-org/react";
+import { useState } from "react";
+import ProductDetailsModal from "./ProductDetailsModal";
+import ProductEditableModal from "./ProductEditableModal";
+
+const Action = {
+  VIEW: "View",
+  UPDATE: "Update",
+};
+
+const ProductCard = ({ product }) => {
+  // control the modals
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [selectedAction, setSelectedAction] = useState(null);
+
+  if (!product) {
+    return <div>Oops! Looks like the product is currently unavailable!</div>;
+  }
+
+  return (
+    <>
+      <div
+        className="rounded border px-8 py-4 shadow-md hover:cursor-pointer"
+        onClick={() => {
+          setSelectedAction(Action.VIEW);
+          onOpen();
+        }}
+      >
+        <h1 className="text-2xl font-semibold tracking-tight">
+          {product?.name}
+        </h1>
+
+        <Button
+          size="sm"
+          color="primary"
+          onPress={() => {
+            setSelectedAction(Action.UPDATE);
+            onOpen();
+          }}
+        >
+          EDIT
+        </Button>
+      </div>
+
+      {/* -------------- Modals -------------- */}
+      {selectedAction === Action.VIEW && (
+        <ProductDetailsModal
+          product={product}
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+        />
+      )}
+
+      {selectedAction === Action.UPDATE && (
+        <ProductEditableModal
+          title="Edit Product"
+          canDelete={true}
+          product={product}
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+        />
+      )}
+    </>
+  );
+};
+
+export default ProductCard;
