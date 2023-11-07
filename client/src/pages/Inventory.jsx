9cd94457c63ae97e18fb13322aae80db09258725
sebastian-nunez <02snunez@gmail.com>
2023-11-07 07:06:57 -0500

    the inventory now fetches all products and displays them in a grid

--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import ProductGrid from "../components/ProductGrid";
+import ProductsAPI from "../services/ProductsAPI";
+
 const Inventory = () => {
-  return (
-    <div>
-      <div className="text-center">
-        <h1 className="text-5xl font-extrabold tracking-tighter">Inventory</h1>
-        <p className="text-gray-500">We're working on it...</p>
+  // state
+  const [products, setProducts] = useState(null);
+
+  // react-query
+  const productsQuery = useQuery(["products"], ProductsAPI.getAllProducts, {
+    onSuccess: (fetchedProducts) => {
+      setProducts(fetchedProducts);
+    },
+  });
+
+  // TODO: make a loading component (skeleton)
+  if (productsQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // TODO: make a proper error component
+  if (productsQuery.isError) {
+    return (
+      <div>
+        <strong>Something went wrong:</strong> {productsQuery?.error?.message}
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      {/* --------------- Filters --------------- */}
+      {/* TODO: create the filtering options */}
+      <div>Filtering Options</div>
+
+      {/* ------------- Product Grid ------------- */}
+      {<ProductGrid products={products} />}
+    </>
   );
 };
 
