17ee62aa562bc571f6127428d715b5a53eed7cd6
sebastian-nunez <02snunez@gmail.com>
2023-11-08 01:56:49 -0500

    Add query keys to constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -7,6 +7,13 @@ export const PRODUCT_TAGS_BASE_URL = "/api/product-tag";
 export const PRODUCTS_BASE_URL = "/api/products";
 export const CATEGORIES_BASE_URL = "/api/categories";
 
+// --------------- query keys ---------------
+export const USERS_QUERY_KEY = "users";
+export const PRODUCTS_QUERY_KEY = "products";
+export const CATEGORIES_QUERY_KEY = "categories";
+export const TAGS_QUERY_KEY = "tags";
+export const PRODUCT_TAG_QUERY_KEY = "product-tag";
+
 // --------------- stale times ---------------
 export const PRODUCT_STALE_TIME_MILLISECONDS = get_milliseconds_from_minutes(5);
 export const CATEGORY_STALE_TIME_MILLISECONDS =
