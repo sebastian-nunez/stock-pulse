4f5c91afe475e24b3f7f9f64e9591e2765f7ddc0
sebastian-nunez <02snunez@gmail.com>
2023-11-07 20:15:02 -0500

    created helper function to get ms to minutes

--- client/src/utils/types.js
+++ client/src/utils/types.js
@@ -13,3 +13,13 @@ export const EMPTY_PRODUCT = {
   category: "",
   tags: [],
 };
+
+/**
+* Helper function to get milliseconds from minutes
+
+ * @param {int} minutes number of minutes
+ * @returns {int} milliseconds
+ */
+export const get_milliseconds_from_minutes = (minutes) => {
+  return minutes * 60 * 1000;
+};
