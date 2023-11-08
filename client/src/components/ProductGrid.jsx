7f7545bc94bcac17cfbf631cf095fb50e7a2b01a
sebastian-nunez <02snunez@gmail.com>
2023-11-07 20:09:42 -0500

    added no results found

--- client/src/components/ProductGrid.jsx
+++ client/src/components/ProductGrid.jsx
@@ -6,8 +6,16 @@ const ProductGrid = ({ products }) => {
     return <div>Loading...</div>;
   }
 
+  if (products?.length === 0) {
+    return (
+      <div className="p-6 text-center text-lg tracking-tight">
+        No results found.
+      </div>
+    );
+  }
+
   return (
-    <>
+    <div className="flex flex-col gap-3 pb-8">
       {/* --------------- Product Rendering --------------- */}
       <div className="grid gap-6 py-6 md:grid-cols-2 lg:grid-cols-3">
         {products?.map((product) => (
@@ -17,8 +25,8 @@ const ProductGrid = ({ products }) => {
 
       {/* --------------- Pagination Controls --------------- */}
       {/* TODO: implement pagination */}
-      <div className="py-3 pb-8">Pagination Controls</div>
-    </>
+      <div className="text-center">Pagination Controls</div>
+    </div>
   );
 };
 
