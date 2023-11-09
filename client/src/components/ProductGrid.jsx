574e3c63ba7cee3fda6f9ebc4e0de76d15c0de5c
sebastian-nunez <02snunez@gmail.com>
2023-11-08 23:44:34 -0500

    added product pagination

--- client/src/components/ProductGrid.jsx
+++ client/src/components/ProductGrid.jsx
@@ -1,6 +1,14 @@
+import { Pagination } from "@nextui-org/react";
+import { useState } from "react";
+import { PRODUCTS_PER_PAGE } from "../utils/constants";
 import ProductCard from "./ProductCard";
 
 const ProductGrid = ({ products }) => {
+  // state
+  const numberOfProducts = products?.length;
+  const numberOfPages = Math.ceil(numberOfProducts / PRODUCTS_PER_PAGE);
+  const [currentPage, setCurrentPage] = useState(1);
+
   if (!products || products?.length === 0) {
     return (
       <div className="p-6 text-center text-lg tracking-tight">
@@ -13,14 +21,32 @@ const ProductGrid = ({ products }) => {
     <div className="flex flex-col gap-3 px-6 pb-8 sm:px-0">
       {/* --------------- Product Rendering --------------- */}
       <div className="grid gap-6 py-6 md:grid-cols-2 lg:grid-cols-3">
-        {products?.map((product) => (
-          <ProductCard key={product.product_id} product={product} />
-        ))}
+        {products
+          ?.slice(
+            // slice the array to only show the products for the current page
+            (currentPage - 1) * PRODUCTS_PER_PAGE,
+            currentPage * PRODUCTS_PER_PAGE,
+          )
+          .map((product) => (
+            <ProductCard key={product.product_id} product={product} />
+          ))}
       </div>
 
       {/* --------------- Pagination Controls --------------- */}
-      {/* TODO: implement pagination */}
-      <div className="text-center">Pagination Controls</div>
+      <div className="flex justify-center ">
+        <div className="rounded-lg border bg-white p-4 drop-shadow-sm">
+          <Pagination
+            showControls
+            total={numberOfPages}
+            initialPage={1}
+            page={currentPage}
+            onChange={(page) => {
+              setCurrentPage(page);
+              window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -19,3 +19,6 @@ export const PRODUCT_STALE_TIME_MILLISECONDS = get_milliseconds_from_minutes(5);
 export const CATEGORY_STALE_TIME_MILLISECONDS =
   get_milliseconds_from_minutes(30);
 export const TAG_STALE_TIME_MILLISECONDS = get_milliseconds_from_minutes(30);
+
+// --------------- misc ---------------
+export const PRODUCTS_PER_PAGE = 18;
