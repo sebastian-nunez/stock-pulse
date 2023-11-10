636551dddb11bd1aaf335a97781cf212dcb14b1a
priscillalynn <plynn9036@gmail.com>
2023-11-10 18:10:59 -0500

    updated homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      <div className="text-center">
-        <h1 className="text-5xl font-extrabold tracking-tighter">Home</h1>
-        <p className="pb-6 text-gray-500">We're working on it...</p>
+    <div className="flex h-screen items-center justify-center md:flex-row">
+      <div className="mr-8 text-left">
+        <h1 className="mr-9 text-7xl font-extrabold tracking-tighter">
+          Inventory <br /> Management
+        </h1>
+        <p className="my-2.5 pb-6 text-gray-500">
+          Managing inventory made effortless!
+        </p>
+        <Link to="/404">
+          <button className="rounded bg-blue-500 font-semibold text-white hover:bg-blue-600 md:px-7 md:py-2.5">
+            Get Started
+          </button>
+        </Link>
+      </div>
+
+      <div>
+        <img
+          src="../src/assets/stockpulseimage.png"
+          alt="inventory management image"
+          className="max-w-lg"
+        />
       </div>
     </div>
   );
