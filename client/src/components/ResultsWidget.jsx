033e3f74ec1b404b6f2e2050a2170bf035644d88
sebastian-nunez <02snunez@gmail.com>
2023-11-09 23:07:43 -0500

    Add ResultsWidget component for displaying search results with pagination and refresh functionality.

---
+++ client/src/components/ResultsWidget.jsx
@@ -0,0 +1,62 @@
+import { Tooltip } from "@nextui-org/react";
+import { RefreshCcw } from "lucide-react";
+import { useQueryClient } from "react-query";
+import {
+  CATEGORIES_QUERY_KEY,
+  PRODUCTS_QUERY_KEY,
+  TAGS_QUERY_KEY,
+} from "../utils/constants";
+
+export const rowsPerPageOptions = [18, 24, 30, 42, 54, 72, 96];
+
+const ResultsWidget = ({
+  rowsPerPage,
+  setRowsPerPage,
+  numberOfResults,
+  changePage,
+}) => {
+  const queryClient = useQueryClient();
+
+  return (
+    <div className="flex items-center justify-between text-small text-default-400">
+      {/* ------------ Results ------------ */}
+      <span aria-label="Number of results">Results: {numberOfResults}</span>
+
+      <div className="flex items-center">
+        {/* ---------- Rows per page ---------- */}
+        <label>Rows per page:</label>
+
+        <select
+          aria-label="Select items per page"
+          value={rowsPerPage}
+          onChange={(e) => setRowsPerPage(Number(e.target.value))}
+          className="bg-transparent"
+        >
+          {rowsPerPageOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+
+        {/* ---------- Refresh Button ---------- */}
+        <Tooltip content="Refresh">
+          <button
+            className="ml-3 rounded-lg p-1 hover:bg-gray-200 active:text-default-500"
+            onClick={() => {
+              queryClient.invalidateQueries([PRODUCTS_QUERY_KEY]);
+              queryClient.invalidateQueries([CATEGORIES_QUERY_KEY]);
+              queryClient.invalidateQueries([TAGS_QUERY_KEY]);
+
+              changePage(1); // reset page to 1
+            }}
+          >
+            <RefreshCcw width={20} height={20} />
+          </button>
+        </Tooltip>
+      </div>
+    </div>
+  );
+};
+
+export default ResultsWidget;
