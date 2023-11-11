a2f09452b677927cb45f263e089db8cbb41ca683
sebastian-nunez <02snunez@gmail.com>
2023-11-11 01:48:07 -0500

    login form is fully validated

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,26 +1,88 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@nextui-org/react";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { loginSchema } from "../utils/schemas";
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+
+  // react-hook-form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ resolver: zodResolver(loginSchema) });
+
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const onSubmit = async (data) => {
+    try {
+      console.log(data);
+      // await AuthAPI.login(data);
+      toast.success("You are now logged in.");
+      navigate("/browser");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+
+    reset();
+  };
+
   return (
-    <form id="login-form" className="flex flex-col gap-5">
+    <form
+      id="login-form"
+      noValidate
+      className="flex flex-col gap-5"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       {/* --------------- Email -------------- */}
       <Input
+        {...register("email")}
         variant="bordered"
         type="email"
         label="Email"
         isClearable
         isRequired
-        className="rounded-xl bg-white"
+        isInvalid={errors.email !== undefined}
+        errorMessage={errors.email?.message}
+        classNames={{
+          inputWrapper: ["bg-white"],
+          helperWrapper: ["bg-transparent"], // error message
+        }}
       />
 
       {/* --------------- Password -------------- */}
       <Input
-        variant="bordered"
-        type="password"
+        {...register("password")}
         label="Password"
-        isClearable
+        variant="bordered"
+        type={isVisible ? "text" : "password"}
         isRequired
-        className="rounded-xl bg-white"
+        isInvalid={errors.password !== undefined}
+        errorMessage={errors.password?.message}
+        endContent={
+          <button
+            type="button"
+            onClick={toggleVisibility}
+            className="flex items-center justify-center focus:outline-none"
+          >
+            {isVisible ? (
+              <Eye height={18} width={18} />
+            ) : (
+              <EyeOff height={18} width={18} />
+            )}
+          </button>
+        }
+        classNames={{
+          inputWrapper: ["bg-white"],
+          helperWrapper: ["bg-transparent"], // error message
+        }}
       />
 
       {/* --------------- Login Button -------------- */}
