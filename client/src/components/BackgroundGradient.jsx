7fc6f923cbeec9c6929852059fd21b0944fb9800
sebastian-nunez <02snunez@gmail.com>
2023-11-12 01:38:32 -0500

    created a background gradient component

---
+++ client/src/components/BackgroundGradient.jsx
@@ -0,0 +1,59 @@
+const BackgroundGradient = ({ variant }) => {
+  const getGradient = (variant) => {
+    switch (variant) {
+      case "primary":
+        return (
+          <>
+            <div className="blob absolute right-0 top-0 -z-10 h-[600px] w-[600px] rounded-[999px] bg-opacity-60 bg-gradient-to-r from-slate-100 via-teal-100 to-blue-300 blur-3xl" />
+
+            <div className="blob  absolute inset-x-1/2 inset-y-1/4 -z-10 h-[40rem] w-[40rem] rounded-full bg-opacity-60 bg-gradient-to-bl from-indigo-200 via-purple-200 to-pink-200 blur-3xl" />
+
+            <div className="blob absolute inset-x-0 inset-y-0 -z-10 h-[30rem] w-[30rem] rounded-full bg-opacity-60 bg-gradient-to-br from-slate-100 via-purple-200 to-blue-300 blur-3xl" />
+
+            <div className="blob absolute bottom-0 left-0 -z-10 h-[50rem] w-[50rem] rounded-[999px] bg-opacity-60 bg-gradient-to-r from-red-200 via-gray-100 to-blue-100 blur-3xl" />
+          </>
+        );
+      case "secondary":
+        return (
+          <>
+            <div className="blob  absolute inset-x-1/2 inset-y-1/4 -z-10 h-[40rem] w-[40rem] rounded-full bg-opacity-60 bg-gradient-to-bl from-indigo-200 via-purple-200 to-pink-200 blur-3xl" />
+
+            <div className="blob absolute inset-x-0 inset-y-0 -z-10 h-[30rem] w-[30rem] rounded-full bg-opacity-60 bg-gradient-to-br from-slate-100 via-purple-200 to-blue-300 blur-3xl" />
+
+            <div className="blob absolute right-0 top-0 -z-10 h-[600px] w-[600px] rounded-[999px] bg-opacity-60 bg-gradient-to-r from-slate-100 via-teal-100 to-blue-300 blur-3xl" />
+
+            <div className="blob absolute bottom-0 left-0 -z-10 h-[50rem] w-[50rem] rounded-[999px] bg-opacity-60 bg-gradient-to-r from-red-200 via-gray-100 to-blue-100 blur-3xl" />
+          </>
+        );
+      case "accent":
+        return (
+          <>
+            <div className="blob absolute inset-x-1/2 inset-y-1/4 -z-10 h-[40rem] w-[40rem] rounded-full bg-opacity-60 bg-gradient-to-bl from-indigo-200 via-purple-200 to-pink-200 blur-3xl" />
+
+            <div className="blob absolute inset-x-0 inset-y-0 -z-10 h-[30rem] w-[30rem] rounded-full bg-opacity-60 bg-gradient-to-br from-slate-100 via-purple-200 to-blue-300 blur-3xl" />
+          </>
+        );
+
+      case "subtle":
+        return (
+          <>
+            <div className="blob absolute right-1/2 top-1/2 -z-10 h-[800px] w-[800px] rounded-[999px] bg-opacity-60 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-300 blur-3xl" />
+
+            <div className="blob absolute -right-1/2 top-1/4 -z-10 h-[800px] w-[800px] rounded-[999px] bg-opacity-60 bg-gradient-to-r from-slate-100 via-teal-100 to-blue-300 blur-3xl" />
+          </>
+        );
+      default:
+        return (
+          <>
+            <div className="blob absolute inset-x-1/2 inset-y-1/4 -z-10 h-[40rem] w-[40rem] rounded-full bg-opacity-60 bg-gradient-to-bl from-indigo-200 via-purple-200 to-pink-200 blur-3xl" />
+
+            <div className="blob absolute inset-x-0 inset-y-0 -z-10 h-[30rem] w-[30rem] rounded-full bg-opacity-60 bg-gradient-to-br from-slate-100 via-purple-200 to-blue-300 blur-3xl" />
+          </>
+        );
+    }
+  };
+
+  return <>{getGradient(variant)}</>;
+};
+
+export default BackgroundGradient;
