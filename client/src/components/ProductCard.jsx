949e86ab4ffea7ab7423855fc6a4728820b27667
sebastian-nunez <02snunez@gmail.com>
2023-11-13 01:53:17 -0500

    images all have a fixed height and width now

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -44,7 +44,7 @@ const ProductCard = ({ product, isLoading = false }) => {
             <img
               src={product?.image}
               alt={product?.name}
-              className="h-60 w-80 rounded-xl object-cover object-center md:w-96"
+              className="h-60 w-96 rounded-xl object-cover object-center"
             />
           ) : (
             <div className="flex h-60 w-full items-center justify-center rounded-xl border drop-shadow-sm">
