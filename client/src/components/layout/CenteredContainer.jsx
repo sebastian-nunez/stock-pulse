f9ba9d1aedc6d47d4f8c91a818826263b38c47b4
sebastian-nunez <02snunez@gmail.com>
2023-11-13 00:53:04 -0500

    removed padding

--- client/src/components/layout/CenteredContainer.jsx
+++ client/src/components/layout/CenteredContainer.jsx
@@ -1,6 +1,6 @@
 const CenteredContainer = ({ children }) => {
   return (
-    <div className="mx-auto flex flex-col gap-4 px-6 sm:px-0 md:w-3/5 xl:w-1/3">
+    <div className="mx-auto flex flex-col gap-4 md:w-3/5 xl:w-1/3">
       {children}
     </div>
   );
