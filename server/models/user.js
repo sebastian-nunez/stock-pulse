6b8ba9f230a098d8d2ac52dfffd05a4687e4eb75
sebastian-nunez <02snunez@gmail.com>
2023-11-14 16:27:16 -0500

    Refactor user model to use new table name and order by id

--- server/models/user.js
+++ server/models/user.js
@@ -4,29 +4,12 @@ class UserModel {
   static getAll = async () => {
     const selectQuery = `
       SELECT *
-      FROM user_table
-      ORDER BY firstname, lastname;
+      FROM users
+      ORDER BY id;
     `;
 
     const results = await pool.query(selectQuery);
     return results.rows;
-  };
-
-  static createOne = async ({ firstname, lastname, role, image }) => {
-    const insertQuery = `
-      INSERT INTO user_table (firstname, lastname, role, image)
-      VALUES ($1, $2, $3, $4)
-      RETURNING *;
-    `;
-
-    const results = await pool.query(insertQuery, [
-      firstname,
-      lastname,
-      role,
-      image
-    ]);
-
-    return results.rows[0];
   };
 }
 
