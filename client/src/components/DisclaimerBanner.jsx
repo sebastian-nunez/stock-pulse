99658d597a53806f35e45fb4d16daa724f6d7b97
sebastian-nunez <02snunez@gmail.com>
2023-11-17 09:21:39 -0500

    added AutoAnimation to disclaimer banner

--- client/src/components/DisclaimerBanner.jsx
+++ client/src/components/DisclaimerBanner.jsx
@@ -1,8 +1,14 @@
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { X } from "lucide-react";
 import { useEffect, useState } from "react";
 
-const DisclaimerBanner = ({ children, durationSeconds = 20 }) => {
-  const [isVisible, setIsVisible] = useState(true);
+const DisclaimerBanner = ({
+  children,
+  durationSeconds = 20,
+  initialDelaySeconds = 0.5,
+}) => {
+  const [isVisible, setIsVisible] = useState(initialDelaySeconds === 0);
+  const [parent, enableAnimations] = useAutoAnimate();
 
   const toggleVisibility = () => setIsVisible(!isVisible);
 
@@ -17,22 +23,36 @@ const DisclaimerBanner = ({ children, durationSeconds = 20 }) => {
     };
   }, []);
 
-  if (!isVisible) return null;
+  // show disclaimer after a delay
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setIsVisible(true);
+    }, initialDelaySeconds * 1000);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
+
   return (
-    <div className="rounded border-l-4 border-yellow-500 bg-yellow-100 p-4 text-sm text-yellow-700 drop-shadow">
-      <h1 className="flex items-center justify-between">
-        <strong>DISCLAIMER</strong>
+    <div ref={parent}>
+      {isVisible ? (
+        <div className="rounded border-l-4 border-yellow-500 bg-yellow-100 p-4 text-sm text-yellow-700 drop-shadow">
+          <h1 className="flex items-center justify-between">
+            <strong>DISCLAIMER</strong>
 
-        <button
-          aria-label="Close banner"
-          onClick={toggleVisibility}
-          className="cursor-pointer text-yellow-700"
-        >
-          <span>{<X height={20} />}</span>
-        </button>
-      </h1>
+            <button
+              aria-label="Close banner"
+              onClick={toggleVisibility}
+              className="cursor-pointer text-yellow-700"
+            >
+              <span>{<X height={20} />}</span>
+            </button>
+          </h1>
 
-      <p>{children}</p>
+          <p>{children}</p>
+        </div>
+      ) : null}
     </div>
   );
 };
